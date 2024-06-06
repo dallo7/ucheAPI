a3714ac2101dfe36146e61d3c69cdc8e32d5e59e
Dalmas Chituyi <122692086+dallo7@users.noreply.github.com>
2024-06-06 11:38:19 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import langchainScrapper
 import Aimodel
 from flask import Flask, request
+import wikipedia.exceptions
+import urllib.error
 
 app = Flask(__name__)
 
@@ -12,23 +14,20 @@ def user():
 
 @app.route("/linkContent", methods=["POST"])
 def create_user():
-    data = request.get_json()
-
-    genLink = data["generateLink"]
-
-    genLink = langchainScrapper.procesLink(genLink)
-    print(genLink)
-
-    genLink = Aimodel.summerize(genLink)
-
-    print(genLink)
-
-    genLink = Aimodel.format_to_points(genLink)
-
-    return genLink
+    try:
+        data = request.get_json()
+        genLink = data["generateLink"]
+        genLink = langchainScrapper.procesLink(genLink)
+        genLink = Aimodel.summerize(genLink)
+        genLink = Aimodel.format_to_points(genLink)
+        return genLink
+    except wikipedia.exceptions.PageError as e:
+        return {"error": "Wikipedia page error: " + str(e)}, 500
+    except urllib.error.URLError as e:
+        return {"error": "URL error: " + str(e)}, 500
+    except Exception as e:
+        return {"error": "An unexpected error occurred: The link exceeds max limit of text" + str(e)}, 500
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
